2078c7f24e33ba36fb8808d28f6b17a6e3a72916
ClazyChen <clazychen@gmail.com>
2019-07-17 16:34:54 +0800

    update directory structure

--- src/main/scala/cpu/execute/branch.scala
+++ src/main/scala/cpu/execute/branch.scala
@@ -44,3 +44,25 @@ class BranchCtrl extends Module {
         )
     )
 }
+
+class Branch extends Module {
+    val io =  IO(new Bundle {
+        val branch_type = Input(UInt(3.W))
+        val input1 = Input(UInt(64.W))
+        val input2 = Input(UInt(64.W))
+        val pc     = Input(UInt(64.W))
+        val imm    = Input(UInt(64.W)) // delta_PC
+
+        val jump   = Output(Bool()) // 是否跳转
+        val jdest  = Output(UInt(64.W)) // 跳转目标
+    })
+
+    val bctrl = Module(new BranchCtrl)
+    
+    bctrl.io.branch_type <> io.branch_type
+    bctrl.io.input1      <> io.input1
+    bctrl.io.input2      <> io.input2
+    bctrl.io.jump        <> io.jump
+
+    io.jdest := io.pc + io.imm
+}
--- src/main/scala/cpu/memoryCtrl.scala
+++ src/main/scala/cpu/memoryCtrl.scala
@@ -3,6 +3,35 @@ package cpu;
 import chisel3._
 import chisel3.util._
 
+object MEMT {
+    val NOP = "b1111".U(4.W)
+    val LB  = "b1000".U(4.W)
+    val LBU = "b1100".U(4.W)
+    val LH  = "b1001".U(4.W)
+    val LHU = "b1101".U(4.W)
+    val LW  = "b1010".U(4.W)
+    val LWU = "b1110".U(4.W)
+    val LD  = "b1011".U(4.W)
+    val SB  = "b0000".U(4.W)
+    val SH  = "b0001".U(4.W)
+    val SW  = "b0010".U(4.W)
+    val SD  = "b0011".U(4.W)
+}
+
+class Memory extends Bundle {
+
+    val mode = Input(UInt(4.W))
+
+    // reader
+    val raddr = Input(UInt(64.W))
+    val rdata = Output(UInt(64.W))
+    
+    // writer
+    val waddr = Input(UInt(64.W))
+    val wdata = Input(UInt(64.W))
+
+}
+
 class MemoryCtrl extends Module {
     val io =  IO(new Bundle {
         val nls  = Input(Bool())
