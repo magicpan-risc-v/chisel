20411b3dbf55e65194913b1460528f64042d7ab4
PanQL <panqinglin00@163.com>
2019-07-20 03:11:13 +0800

    add csr definition

---
+++ src/main/scala/cpu/CSR.scala
@@ -0,0 +1,91 @@
+package cpu;
+
+import chisel3._
+import chisel3.util._
+
+class CSR extends Module {
+  val io = IO(new Bundle {
+  })
+
+  val csr = Mem(0x400, UInt(32.W))  // all csrs
+  object ADDR {
+    // M Info
+    val mvendorid = "hF11".U
+    val marchid   = "hF12".U
+    val mimpid    = "hF13".U
+    val mhartid   = "hF14".U
+    //M Trap Setup
+    val mstatus   = "h300".U
+    val misa      = "h301".U
+    val medeleg   = "h302".U
+    val mideleg   = "h303".U
+    val mie       = "h304".U
+    val mtvec     = "h305".U
+    val mcounteren= "h306".U
+    //M Trap Hangding
+    val mscratch  = "h340".U
+    val mepc      = "h341".U
+    val mcause    = "h342".U
+    val mtval     = "h343".U
+    val mip       = "h344".U
+    //S Trap Setup
+    val sstatus   = "h100".U
+    val sedeleg   = "h102".U
+    val sideleg   = "h103".U
+    val sie       = "h104".U
+    val stvec     = "h105".U
+    val scounteren= "h106".U
+    //S Trap Hangding
+    val sscratch  = "h140".U
+    val sepc      = "h141".U
+    val scause    = "h142".U
+    val stval     = "h143".U
+    val sip       = "h144".U
+    // S Protection and Translation
+    val satp      = "h180".U
+    // U Trap Setup
+    val utvec     = "h005".U
+    // U Trap Hangding
+    val uscratch  = "h040".U
+    val uepc      = "h041".U
+    val ucause    = "h042".U
+    val utval     = "h043".U
+    val uip       = "h044".U
+    // emmmm..
+    val mtimecmp  = "h321".U
+    val mtimecmph = "h322".U
+    // time
+    val time      = "hC01".U
+    val timeh     = "hC81".U
+    val cycle     = "hC00".U
+    val cycleh    = "hC80".U
+  }
+
+  class MStatus extends Bundle {
+    val SD = Bool()
+    val zero1 = UInt(27.W)
+    val SXL = UInt(2.W)
+    val UXL = UInt(2.W)
+    val TSR = Bool()
+    val zero2 = UInt(9.W)
+    val TSR = Bool()
+    val TW = Bool()
+    val TVM = Bool()
+    val MXR = Bool()
+    val SUM = Bool()
+    val MPriv = Bool()
+    val XS = UInt(2.W)
+    val FS = UInt(2.W)
+    val MPP = UInt(2.W)
+    val old_HPP = UInt(2.W)
+    val SPP = UInt(1.W)
+    val MPIE = Bool()
+    val old_HPIE = Bool()
+    val SPIE = Bool()
+    val UPIE = Bool()
+    val MIE = Bool()
+    val old_HIE = Bool()
+    val SIE = Bool()
+    val UIE = Bool()
+  }
+}
