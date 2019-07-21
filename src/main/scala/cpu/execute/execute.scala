d74bd8ea199198ab1a8b01dca3831911659c0476
ClazyChen <clazychen@gmail.com>
2019-07-21 16:26:43 +0800

    LUI & AUIPC (all non-sys instructions completed)

--- src/main/scala/cpu/execute/execute.scala
+++ src/main/scala/cpu/execute/execute.scala
@@ -28,16 +28,17 @@ class Execute extends Module {
     val los = Module(new LoadStore)
     val bra = Module(new Branch)
     val rsl = Module(new RegSelector)
-    
+
     val alu_inputB = Mux(rsl.io.sreg.rs2_valid, rsl.io.sreg.rs2_value, io.imm)
     val bvalid = io.exe_type === EXT.BRANCH
     val jvalid = io.exe_type === EXT.JUMP
     val nls  = io.exe_type === EXT.LOS
     val wbrv = rsl.io.sreg.rd_valid && !nls // 是否在EX阶段取到了写回值
-    val wbrd = Mux(jvalid, 
-        Mux(rsl.io.sreg.rd_index === 0.U, 0.U(64.W), io.pc + 4.U),
-        alu.io.output
-    )
+    val wbrd = MuxLookup(io.exe_type, alu.io.output, Seq(
+        EXT.JUMP  -> Mux(rsl.io.sreg.rd_index === 0.U, 0.U(64.W), io.pc + 4.U),
+        EXT.LUI   -> io.imm,
+        EXT.AUIPC -> (io.imm + io.pc)
+    ))
     val jump = bra.io.jump || jvalid
     val jdest = Mux(jvalid, 
         Mux(rsl.io.sreg.rs1_valid, rsl.io.sreg.rs1_value + io.imm, io.pc + io.imm),
--- tests/test1.s
+++ tests/test1.s
@@ -1,9 +1,55 @@
 .global _start
 _start:
 	li x10, 10
-cyc:
-	addi x10,x10,-1
-	beq x10,x0,end
-	bne x10,x0,cyc
-end:
-	li x10,10
+	li x2, 0
+	li x1, 0
+_SUM:
+	addi x1, x1, 1
+	add  x2, x2, x1
+	bne  x1, x10, _SUM
+check_x2:
+	li x4, 55
+    la a0, x2_wrong
+    bne x4, x2, fail
+test2:
+	li x4, 1000
+	li x5, 1
+	li x6, 1
+_FI:
+	add x5, x5, x6
+	bge x5,x4, _END5
+	add x6, x5,x6
+	bge x6,x4, _END6
+	j _FI
+_END5:
+	mv x7, x5
+	j _END
+_END6:
+	mv x7,x6
+	j _END
+_END:
+
+check:
+    li x8, 1000
+    la a0, x4_wrong
+    bne x4, x8, fail
+    li x8, 1597
+    la a0, x5_wrong
+    bne x5, x8, fail
+    li x8, 987
+    la a0, x6_wrong
+    bne x6, x8, fail
+    li x8, 1597
+    la a0, x7_wrong
+    bne x7, x8, fail
+pass:
+	lui x31, 0xcafe
+fail:
+    lui x31, 0xdead
+
+x2_wrong: .string "x2 != 55\0"
+x4_wrong: .string "x4 wrong\0"
+x5_wrong: .string "x5 wrong\0"
+x6_wrong: .string "x6 wrong\0"
+x7_wrong: .string "x7 wrong\0"
+unknown:  .string "unknown reason fail\0"
